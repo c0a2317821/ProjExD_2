c8b908dad56f939c7433d3e3213ffdd294bc679a
Ryu <c0a2317821@edu.teu.ac.jp>
2024-04-23 14:41:51 +0900

    練習2 : 爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,26 +1,35 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1000, 600
-DELTA = { #移動量辞書
-    pg.K_UP: (0,-5),
-    pg.K_DOWN: (0,+5),
-    pg.K_LEFT: (-5,0),
-    pg.K_RIGHT: (+5,0),
-    }
-
+DELTA = {  # 移動量辞書（押下キー：移動量タプル）
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    # ここからこうかとんの設定
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    # ここから爆弾の設定
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 横方向速度，縦方向速度
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -29,15 +38,18 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in DELTA.items():
-            if key_lst[k]: 
-                sum_mv[0] += v [0]
-                sum_mv[1] += v [1]
-        
+            if key_lst[k]:
+               sum_mv[0] += v[0]
+               sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        # 爆弾の移動と表示
+        bd_rct.move_ip(vx, vy)       
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
